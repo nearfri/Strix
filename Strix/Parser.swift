98e6161d4d10a38fce4954148faea2aefab757cc
Ukjeong Lee <nearfri@gmail.com>
2017-05-22 01:43:15 +0900

    Parser.map, .flatMap 추가

--- Strix/Parser.swift
+++ Strix/Parser.swift
@@ -5,7 +5,32 @@ public struct Parser<T> {
     public init(_ parse: @escaping (CharacterStream) -> Reply<T>) {
         self.parse = parse
     }
+}
+
+extension Parser {
+    public func flatMap<U>(_ transform: @escaping (T) -> Parser<U>) -> Parser<U> {
+        return Parser<U>({ (stream) -> Reply<U> in
+            switch self.parse(stream) {
+            case let .failure(e):
+                return .failure(e)
+            case let .fatalFailure(e):
+                return .fatalFailure(e)
+            case let .success(v, e):
+                let stateTag = stream.stateTag
+                let reply = transform(v).parse(stream)
+                return stateTag == stream.stateTag && !e.isEmpty ? reply.prepending(e) : reply
+            }
+        })
+    }
     
+    public func map<U>(_ transform: @escaping (T) -> U) -> Parser<U> {
+        return flatMap { (v) in
+            return Parser<U> { _ in .success(transform(v), []) }
+        }
+    }
+}
+
+extension Parser {
     public func run(_ string: String) -> ParseResult<T> {
         let stream = CharacterStream(string: string)
         switch parse(stream) {
--- StrixTests/ParserTests.swift
+++ StrixTests/ParserTests.swift
@@ -2,6 +2,11 @@
 import XCTest
 @testable import Strix
 
+private enum DummyError: Error {
+    case err0
+    case err1
+}
+
 class ParserTests: XCTestCase {
     override func setUp() {
         super.setUp()
@@ -9,6 +14,123 @@ class ParserTests: XCTestCase {
     
     override func tearDown() {
         super.tearDown()
+    }
+    
+    func test_flatMap_success() {
+        let numberString = Parser { (stream) -> Reply<String> in
+            return .success("2", [DummyError.err0])
+        }
+        let toInt = { (str: String) -> Parser<Int> in
+            return Parser { (stream) -> Reply<Int> in
+                return .success(Int(str)!, [DummyError.err1])
+            }
+        }
+        let p = numberString.flatMap(toInt)
+        let stream = CharacterStream(string: "")
+        let reply = p.parse(stream)
+        XCTAssertEqual(reply.value, 2)
+    }
+    
+    func test_flatMap_whenChangesStateTag_notPrependingErrors() {
+        let numberString = Parser { (stream) -> Reply<String> in
+            return .success("2", [DummyError.err0])
+        }
+        let toInt = { (str: String) -> Parser<Int> in
+            return Parser { (stream) -> Reply<Int> in
+                stream.stateTag += 1
+                return .success(Int(str)!, [DummyError.err1])
+            }
+        }
+        let p = numberString.flatMap(toInt)
+        let stream = CharacterStream(string: "")
+        let reply = p.parse(stream)
+        XCTAssertEqual(reply.errors as! [DummyError], [DummyError.err1])
+    }
+    
+    func test_flatMap_whenNotChangeStateTag_prependingErrors() {
+        let numberString = Parser { (stream) -> Reply<String> in
+            return .success("2", [DummyError.err0])
+        }
+        let toInt = { (str: String) -> Parser<Int> in
+            return Parser { (stream) -> Reply<Int> in
+                return .success(Int(str)!, [DummyError.err1])
+            }
+        }
+        let p = numberString.flatMap(toInt)
+        let stream = CharacterStream(string: "")
+        let reply = p.parse(stream)
+        XCTAssertEqual(reply.errors as! [DummyError], [DummyError.err0, DummyError.err1])
+    }
+    
+    func test_flatMap_failure() {
+        let alwaysFail = Parser { (stream) -> Reply<String> in
+            return .failure([DummyError.err0])
+        }
+        let toInt = { (str: String) -> Parser<Int> in
+            return Parser { (stream) -> Reply<Int> in
+                XCTFail()
+                return .success(Int(str)!, [DummyError.err1])
+            }
+        }
+        let p = alwaysFail.flatMap(toInt)
+        let stream = CharacterStream(string: "")
+        let reply = p.parse(stream)
+        if case let .failure(errors) = reply {
+            XCTAssertEqual(errors as! [DummyError], [DummyError.err0])
+        } else {
+            XCTFail()
+        }
+    }
+    
+    func test_flatMap_fatalFailure() {
+        let alwaysFail = Parser { (stream) -> Reply<String> in
+            return .fatalFailure([DummyError.err0])
+        }
+        let toInt = { (str: String) -> Parser<Int> in
+            return Parser { (stream) -> Reply<Int> in
+                XCTFail()
+                return .success(Int(str)!, [DummyError.err1])
+            }
+        }
+        let p = alwaysFail.flatMap(toInt)
+        let stream = CharacterStream(string: "")
+        let reply = p.parse(stream)
+        if case let .fatalFailure(errors) = reply {
+            XCTAssertEqual(errors as! [DummyError], [DummyError.err0])
+        } else {
+            XCTFail()
+        }
+    }
+    
+    func test_map_success() {
+        let numberString = Parser { (stream) -> Reply<String> in
+            return .success("2", [DummyError.err0])
+        }
+        let toInt = { (str: String) -> Int in
+            return Int(str)!
+        }
+        let p = numberString.map(toInt)
+        let stream = CharacterStream(string: "")
+        let reply = p.parse(stream)
+        XCTAssertEqual(reply.value, 2)
+    }
+    
+    func test_map_failure() {
+        let alwaysFail = Parser { (stream) -> Reply<String> in
+            return .failure([DummyError.err0])
+        }
+        let toInt = { (str: String) -> Int in
+            XCTFail()
+            return Int(str)!
+        }
+        let p = alwaysFail.map(toInt)
+        let stream = CharacterStream(string: "")
+        let reply = p.parse(stream)
+        if case let .failure(errors) = reply {
+            XCTAssertEqual(errors as! [DummyError], [DummyError.err0])
+        } else {
+            XCTFail()
+        }
     }
     
     func test_run_whenSuccess_returnSuccess() {
