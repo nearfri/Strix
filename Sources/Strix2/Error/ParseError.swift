2cd9641e57df6a7365026b70f26e1546097348eb
Ukjeong Lee <nearfri@gmail.com>
2020-11-26 01:11:19 +0900

    ParseError.description 추가

--- Sources/Strix2/Error/ParseError.swift
+++ Sources/Strix2/Error/ParseError.swift
@@ -9,3 +9,24 @@ public enum ParseError: Error, Equatable {
     case nested(position: String.Index, errors: [ParseError])
     case compound(label: String, position: String.Index, errors: [ParseError])
 }
+
+extension ParseError: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case let .expected(label):
+            return "expected(label: \"\(label)\")"
+        case let .unexpected(label):
+            return "unexpected(label: \"\(label)\")"
+        case let .expectedString(string, caseSensitive):
+            return "expectedString(string: \"\(string)\", caseSensitive: \(caseSensitive))"
+        case let .unexpectedString(string, caseSensitive):
+            return "unexpectedString(string: \"\(string)\", caseSensitive: \(caseSensitive))"
+        case let .generic(message):
+            return "generic(message: \"\(message)\")"
+        case let .nested(_, errors):
+            return "nested(errors: \(errors))"
+        case let .compound(label, _, errors):
+            return "compound(label: \"\(label)\", errors: \(errors))"
+        }
+    }
+}
