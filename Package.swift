fa609d89db9647844ce7fc23e525564374d039c7
Ukjeong Lee <nearfri@gmail.com>
2020-11-30 09:37:27 +0900

    Strix v1 제거

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
     name: "Strix",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(name: "Strix", targets: ["Strix"]),
         .library(name: "Strix2", targets: ["Strix2"]),
         .library(name: "StrixParsers", targets: ["StrixParsers"]),
     ],
@@ -18,13 +17,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Strix",
-            dependencies: []),
-        .testTarget(
-            name: "StrixTests",
-            dependencies: ["Strix"]),
-        
         .target(
             name: "Strix2",
             dependencies: []),
